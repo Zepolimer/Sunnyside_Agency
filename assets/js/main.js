0bbfcc16de75c4e4ec68604664f6c11b0701ef5c
Rémi Lopez <76875802+Zepolimer@users.noreply.github.com>
2021-07-08 15:53:55 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,9 @@ let menuNav = document.querySelector(".menuWrapper");
 let burgerBtn = document.querySelector(".burgerMenu");
 
 burgerBtn.addEventListener('click', () => {
-    if(menuNav.style.display==="none") {
-        menuNav.style.display="flex";
-    } else {
+    if(menuNav.style.display==="flex") {
         menuNav.style.display="none";
+    } else {
+        menuNav.style.display="flex";
     }
 })
